19b30925d5385cc1b19d8e8dd1b0ba577d98aa62
Artur <artur167950@yandex.ru>
2020-10-18 16:03:39 +0300

    init first commit

---
+++ main.py
@@ -0,0 +1,15 @@
+
+def print_hi(name):
+    aTuple = (10, 20, 30, 40)
+    print(aTuple)
+
+    a = aTuple[0]
+    b = aTuple[1]
+    c = aTuple[2]
+    d = aTuple[3]
+
+    print(a, b, c, d)
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    print_hi('PyCharm')
