c6fe1c104d3d5526ac12f0566bc1ea3cc86e59fa
Artur <artur167950@yandex.ru>
2020-10-18 17:06:18 +0300

    add first_2

---
+++ First_2.py
@@ -0,0 +1,10 @@
+def compare():
+    tupl_1=(45,45,45,45)
+    print(tupl_1)
+    if tupl_1[0] == tupl_1[1] and tupl_1[0] == tupl_1[2] and tupl_1[0] == tupl_1[3]:
+        return True
+    else : False
+
+
+
+
