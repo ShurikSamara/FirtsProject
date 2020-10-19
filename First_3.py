b7dc9176323e5d536880d61ab32ee7b1c43972a7
Shurik Samara <shuriksamara@gmail.com>
2020-10-19 22:13:35 +0300

    add decision of the third task

--- First_3.py
+++ First_3.py
@@ -1,6 +1,11 @@
 def delete_empty_tuple():
     aList = [(), (), ('',), ('a', 'b'), ('a', 'b', 'c'), ('d')]
     print(aList)
+    for i in aList:
+        if len(i) == 0:
+            aList.remove(i)
+        else:
+            print(i)
 
 
 delete_empty_tuple()
