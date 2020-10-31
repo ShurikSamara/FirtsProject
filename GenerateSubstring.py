27bbf0b2873fc5f7b1969de8508a501c1d969744
Shurik Samara <shuriksamara@gmail.com>
2020-10-31 18:13:44 +0300

    reversed string

--- GenerateSubstring.py
+++ GenerateSubstring.py
@@ -1,3 +1,20 @@
+# Вывести все подстроки исходной строки sourceString
 def generate_substring():
     sourceString = "Съешь ещё этих мягких французских булок, да выпей чаю"
+    print(sourceString)
+    startIndex = 0
 
+    while startIndex < len(sourceString):
+        print(sourceString[startIndex:len(sourceString)])
+        startIndex += 1
+
+    invertString = ''.join(reversed(sourceString))
+    endIndex = 1
+
+    while endIndex < len(invertString):
+        print(invertString[0:endIndex])
+        endIndex += 1
+    print(invertString)
+
+
+generate_substring()
